1301bb8dd8af8fee436b61f3441c8b0a2bc2345b
danielwhz1 <54701487+danielwhz1@users.noreply.github.com>
2024-03-02 11:29:13 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -247,6 +247,14 @@ const BLOG = {
   MUSIC_PLAYER_ORDER: process.env.NEXT_PUBLIC_MUSIC_PLAYER_ORDER || 'list', // 默认播放方式，顺序 list，随机 random
   MUSIC_PLAYER_AUDIO_LIST: [
     // 示例音乐列表。除了以下配置外，还可配置歌词，具体配置项看此文档 https://aplayer.js.org/#/zh-Hans/
+    
+    {
+      name: 'S.T.A.Y.',
+      artist: 'Hans Zimmer',
+      url: 'https://music.163.com/song/media/outer/url?id=477933827.mp3',
+      cover:
+            'https://images.app.goo.gl/drQJGJS9MmYWe1yy8.jpg'
+    },
     {
       name: '风を共に舞う気持ち',
       artist: 'Falcom Sound Team jdk',
